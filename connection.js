394efc8d8f162dcbe4a70ada79c3a391ac424356
SampathK-design <110811778+SampathK-design@users.noreply.github.com>
2022-08-31 17:55:28 +0530

    Add files via upload

---
+++ connection.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+
+const DB = process.env.DATABASE 
+
+const options = {
+  useNewUrlParser:true, 
+  useUnifiedTopology:true
+}
+mongoose.connect(DB, options)
+
+const db = mongoose.connection
+db.on("error", console.error.bind(console, "connection error: "));
+db.once('open', function(){
+  console.log("Connection Successful")
+})
