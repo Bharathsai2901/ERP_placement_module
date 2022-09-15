a64497d6d3d9245795c8347498fd6892dac96caa
Akshara-333 <91304813+Akshara-333@users.noreply.github.com>
2022-09-15 12:17:18 +0530

    frontend

---
+++ src/Company_login.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./styles/Company_login.css";
+import { Outlet, Link } from "react-router-dom";
+
+
+export default function Login() {
+  return (
+    <div>
+      <div className="wrapper">
+        <div>
+          <div className="title">Company Login</div>
+          <form action="#">
+            <div className="field">
+              <input type="text" required />
+              <label>Email Address</label>
+            </div>
+            <div className="field">
+              <input type="password" required />
+              <label>Password</label>
+            </div>
+            <div className="content">
+              <div className="checkbox">
+                <input type="checkbox" id="remember-me" />
+                <label for="remember-me">Remember me</label>
+              </div>
+              <div className="pass-link">
+                <a href="#">Forgot password?</a>
+              </div>
+            </div>
+            <div className="field">
+            <Link to="/Company_home"><input type="submit" value="Login" /></Link>
+            </div>
+            {/* <div className="signup-link">
+              Not a member? <a href="#">Signup now</a>
+            </div> */}
+          </form>
+        </div>
+      </div>
+      <Outlet />
+    </div>
+  );
+}
