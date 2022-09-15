84472ff7ed14b9f5e7932b978fb3b2f77dcd6dc1
Akshara-333 <91304813+Akshara-333@users.noreply.github.com>
2022-09-16 02:43:10 +0530

    Update success.js

--- src/Components/success.js
+++ src/Components/success.js
@@ -8,7 +8,7 @@ export default class Success extends Component {
         <div className="card-body text-center pt-5 pb-5">
             <i className="fas fa-check-circle fa-7x text-success"></i>
             <h2>Hang Tight while your Resume Downloads!</h2>
-            <br/> 
+            <br/>
         </div>  
         
       </div>
