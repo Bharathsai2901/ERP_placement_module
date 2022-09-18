3c8f2b279a79b5b3dc7ce6555fa00abc649ffce2
hiranmai19 <cs20b014@iittp.ac.in>
2022-09-19 01:07:03 +0530

    commit6

--- src/Components/reviewapplication.js
+++ src/Components/reviewapplication.js
@@ -1,60 +1,43 @@
 import React from "react";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import CardActions from "@material-ui/core/CardActions";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Grid from "@material-ui/core/Grid";
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import { Link } from "react-router-dom";
-  
+import { flexbox } from "@mui/system";
 
 export default function App() {
     return (
-        <div style={{
-
-        }}>
-            <h4 style={{ padding:20,
+        <div>
+        <h4 style={{ padding:20,
                 textAlignVertical: 'center',
                 textAlign:'center'}}> Review Application</h4>
-            <Card
-                style={{
-                    width: 1300,
-                    marginLeft: 100,
-                    boxShadow: 100,
-                    backgroundColor: "white",
-                }}
-            >
-                <CardContent>
-                    <Typography variant="h5" component="p">
-                        Details:
-                    </Typography>
-                    <Typography
-                        style={{
-                            marginBottom: 12,
-                        }}
-                        color="textSecondary"
-                    >
-                            Number of positions need to be filled : 10<br/>
-                            Number of students applied : 100<br/>
-                            <b>
-                            Exam Details :
-                            </b><br/>
-                            Timings : 25th September 2022<br/>
-                            Mode : Online Test<br/>
-                            Proctors: Hiranmai , Akshara , BharatSai , Roshan , Sampath<br/>
-                            <b>Canditates :</b><br/>
-                            Canditates qualified for 1st round : --<br/>
-                            Canditates qualified for 2nd round : --<br/>
-                            Canditates qualified for final round : --<br/>
-                    </Typography>
-                </CardContent>
-                <CardActions >
-                    <Button style={{
-                    Align : 'center',
-                    colour :'blue',size:"small" }}>
-                    Back to home<Link to="/Company-home"></Link></Button>
-                    
-                </CardActions>
-            </Card>
+        <Card style={{
+            width: 1300,
+            marginLeft: 100,
+            boxShadow: 100,
+            backgroundColor: "white",
+        }}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Kalavandla Hiranmai
+        </Typography>
+      </CardContent>
+      
+      <CardActions>
+      <Grid container spacing={2}>
+      <Button size="small">Experiance</Button>
+        <Button size="small">Assesment</Button>
+        <Button size="small">Availability</Button>
+        </Grid>
+        <Grid container justify="flex-end">
+        <Button variant="contained" >View profile</Button>
+        </Grid>
+      </CardActions>
+    </Card>
         </div>
+        
     );
 }
