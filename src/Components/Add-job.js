b7aacbf2c16bd0ae56ecf57c1e970cc35fe917a0
Bharat Sai <85835366+Bharathsai2901@users.noreply.github.com>
2022-09-18 23:32:45 +0530

    Update Add-job.js

--- src/Components/Add-job.js
+++ src/Components/Add-job.js
@@ -1,87 +1,129 @@
-import React, { useEffect } from "react";
-import { useState } from 'react';
-import { Outlet, Link } from "react-router-dom";
-import Jobdes from "./job_des"
+import React from 'react';
+import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import "../styles/apply.css";
-import storage from "../firebase"
-import {ref, uploadBytes, listAll, getDownloadURL} from "firebase/storage"
-import { v4 } from "uuid"
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Navbar from "./company-nav"
+import MyVerticallyCenteredModal from "./modal"
+// import Avatar from 'react-avatar';
+import "../styles/profile.css"
 
-export default function Form() {
-  const [studentDetails, setStudentDetails] = useState({
-    availability:"", 
-    internships:"", 
-    workDone:""
-  })
+export default function Profile(){
+    const [modalShow, setModalShow] = React.useState(false);
+    return(
+        <div>
+            <Navbar/>
+            <div className="container  page__container">
+                <div className="card border-0 card-form">
+                    <div className="row no-gutters ">
+                        <div className="col-lg-4 card-body">
+                            <p><strong className="headings-color">Job Details</strong></p>
+                            <p className="text-muted mb-0">Please enter relevant details of the job.</p>
+                        </div>
+                        <div className="col-lg-8 card-form__body card-body">
+                            <div className="row ">
+                                <div className="col">
+                                    <div className="form-group">
+                                        <label for="fname">Job title</label>
+                                        <input id="fname" type="text" className="form-control" placeholder="Job title"  />
+                                    </div>
+                                </div>
+                                <div className="col">
+                                    <div className="form-group">
+                                        <label for="lname">Salary</label>
+                                        <input id="lname" type="text" className="form-control" placeholder="Salary"  />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row ">
+                                <div className="col">
+                                    <div className="form-group">
+                                        <label for="fname">Location</label>
+                                        <input id="fname" type="text" className="form-control" placeholder="Location"  />
+                                    </div>
+                                </div>
+                                <div className="col">
+                                    <div className="form-group">
+                                        <label for="lname" className="type">Type</label><br />
+                                        <select id="lname" className="custom-select w-auto">
+                                        <option value="Type">Full time</option>
+                                        <option value="Type">Intern</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row ">
+                                <div className="col">
+                                        <div className="form-group">
+                                            <label for="fname">Openings</label>
+                                            <input id="fname" type="text" className="form-control" placeholder="Openings"  />
+                                        </div>
+                                </div>
+                                <div className="col">
+                                        <div className="form-group">
+                                            <label for="fname">Apply before</label>
+                                            <input id="fname" type="text" className="form-control" placeholder="Apply before"  />
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <label for="desc">Job Description</label>
+                                <textarea id="desc" rows="4" className="form-control" placeholder="Job description"></textarea>
+                            </div>
+                            
+                            
+                        </div>
+                    </div>
+                </div>
 
-  const folderRef = ref(storage, "images/")
-  const [updatedResume, setUpdatedResume] = useState([])
+                
 
-  const [unlockBtn, setUnlockBtn] = useState(false)
+                <div className="card border-0 card-form">
+                    <div className="row  no-gutters">
+                        <div className="col-lg-4 card-body">
+                            <p><strong className="headings-color">Other Details</strong></p>
+                            <p className="text-muted mb-0">Please fill the extra details</p>
+                        </div>
+                        <div className="col-lg-8 card-form__body card-body">
+                            <div className="form-group">
+                                <label>Relevant Documents(if any)</label>
+                                <div className="dz-clickable media align-items-center" data-toggle="dropzone" data-dropzone-url="http://" data-dropzone-clickable=".dz-clickable" data-dropzone-files="[&quot;assets/images/account-add-photo.svg&quot;]">
+                                    <div className="dz-preview dz-file-preview dz-clickable mr-3">
+                                        <div className="avatar avatar-lg">
+                                            <img src="https://lema.frontted.com/assets/images/account-add-photo.svg" className="avatar-img rounded" alt="..." data-dz-thumbnail="" />
+                                        </div>
+                                    </div>
+                                    <div className="media-body">
+                                        <button className="btn btn-sm btn-primary dz-clickable">Browse</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <label for="desc2">Skills Required</label>
+                                <textarea id="desc2" rows="4" className="form-control" placeholder="Skills required"></textarea>
+                            </div>
+                            <div className="form-group">
+                                <label for="desc2">Who can apply</label>
+                                <textarea id="desc2" rows="4" className="form-control" placeholder="Who can apply"></textarea>
+                            </div>
+                           
+                            
+                        </div>
+                    </div>
+                </div>
+                <div className="text-right mb-5">
+                    {/* <a href="" className="btn btn-success">Add</a> */}
 
-  const handleInput = async (event)=>{
-    let name, value 
-    name = event.target.name 
-    if(name === "fileUpload"){
-      value = event.target.files[0]
-      const imageRef = ref(storage, `images/${value.name + v4()}`);
-      const snapshot = await uploadBytes(imageRef, value)
-      getDownloadURL(snapshot.ref).then((url)=>{
-        setUpdatedResume((preState)=>[...preState, url])
-        console.log("Got File!")
-        setUnlockBtn(true)
-      })
-    }
-    else{
-      value = event.target.value
-    }
-    setStudentDetails({...studentDetails, [name]:value})
-  }
+                    <Button className="btn btn-success" onClick={() => setModalShow(true)}>
+                    Add
+                </Button>
 
-  const submitResume = async (event)=>{
-    event.preventDefault()
-    const {availability, internships, workDone} = studentDetails
-    try{
-    const res = await fetch("/uploadResume", {
-      method:"post", 
-      headers:{
-        "Content-Type":"application/json"
-      }, 
-      body:JSON.stringify({availability, internships, workDone, studentResume:updatedResume})
-    })
-    console.log(res.status)
-  }
-  catch(err){
-    console.log(err.message)
-  }
-  }
-
-  return (
-      <div class="form-row align-items-center ">
-          <h1 className="h1text">Job Application</h1>
-          <form className="form-box">
-          <div class="form-group">
-          <label className="form-label" for="textarea"><p className="ques">Have you done any internships ?If yes please mention the details.</p></label> 
-          <textarea id="textarea" name="internships" cols="40" rows="5" required="required" class="form-control" onChange = {handleInput}></textarea>
-          </div>
-          <div class="form-group">
-          <label className="form-label" for="textarea1"><p className="ques">Are you available for 6 months ,starting immediately, for a full time work from home internship?If not,what is time period you are available for and the earliest date you can start this internship on ?</p></label> 
-          <textarea id="textarea1" name="availability" cols="40" rows="5" class="form-control" onChange = {handleInput}></textarea>
-          </div>
-          <div class="form-group">
-          <label className="form-label" for="textarea2"><p className="ques">Kindly give the details of sample applications developed by you.Please share the links if you have them.</p></label> 
-          <textarea id="textarea2" name="workDone" cols="40" rows="5" class="form-control" onChange = {handleInput}></textarea>
-          </div> 
-            <div class="form-group">
-            <label className="form-label" ><p className="ques">Please upload your resume here</p></label><br/>
-            <input className="input" type="file" name = "fileUpload" onChange = {handleInput}/><br/><br/>
-
-            {unlockBtn && <button type="submit" class="btn btn-success"  onClick = {submitResume}>Upload and submit</button>}
+                <MyVerticallyCenteredModal
+                    show={modalShow}
+                    onHide={() => setModalShow(false)}
+                />
+                </div>
             </div>
-            <div >
-            </div>
-          </form>
         </div>
-  );
+    )
 }
