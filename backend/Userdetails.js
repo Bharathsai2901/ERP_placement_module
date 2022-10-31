334219847d6882b6c301fbe4b033120558a505b4
Bharathsai2901 <bharatsaipadala@gmail.com>
2022-10-31 16:37:47 +0530

    Updates

--- backend/Userdetails.js
+++
@@ -1,6 +0,0 @@
-const user = 'cs20b010@iittp.ac.in';
-const pass = 'GroshanCS10*#';
-
-
-exports.user = user
-exports.pass = pass 
---
+++ precache-manifest.8d710ae5eb7b6afe0f2eb3155c8de974.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "81e452a05a2f7f5f211e",
+    "url": "/ERP_placement_module/static/css/main.59672c07.chunk.css"
+  },
+  {
+    "revision": "81e452a05a2f7f5f211e",
+    "url": "/ERP_placement_module/static/js/main.81e452a0.chunk.js"
+  },
+  {
+    "revision": "fbb89998ee840484cbf9",
+    "url": "/ERP_placement_module/static/css/1.75d4a2fc.chunk.css"
+  },
+  {
+    "revision": "fbb89998ee840484cbf9",
+    "url": "/ERP_placement_module/static/js/1.fbb89998.chunk.js"
+  },
+  {
+    "revision": "05ec555aaab36698a2ec",
+    "url": "/ERP_placement_module/static/js/2.05ec555a.chunk.js"
+  },
+  {
+    "revision": "ee0d599dc90e4e5e7c69",
+    "url": "/ERP_placement_module/static/js/3.ee0d599d.chunk.js"
+  },
+  {
+    "revision": "662d538662794760481d",
+    "url": "/ERP_placement_module/static/js/4.662d5386.chunk.js"
+  },
+  {
+    "revision": "61194b7c0267b9a2f632",
+    "url": "/ERP_placement_module/static/js/runtime~main.61194b7c.js"
+  },
+  {
+    "revision": "974119476175087bd351426d2f6fa038",
+    "url": "/ERP_placement_module/index.html"
+  }
+];
